bd21c3ad525f24df115c29e28b084c36dd890d8f
vaibhav rehapade <vaibhav.rehapade@gmail.com>
2023-07-17 19:57:30 +0530

    first commit

---
+++ src/main/java/com/vthinkDynamic/parallelasynchronous/ParallelAsynchronousUsingJavaApplication.java
@@ -0,0 +1,13 @@
+package com.vthinkDynamic.parallelasynchronous;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParallelAsynchronousUsingJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParallelAsynchronousUsingJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vthinkDynamic/parallelasynchronoususingjava/ParallelAsynchronousUsingJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vthinkDynamic.parallelasynchronoususingjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParallelAsynchronousUsingJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
